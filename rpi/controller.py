9b732008b1ee4b71200dd039344c7fec51f26d74
pi <j.r@alaska.edu>
2019-10-12 23:20:11 +0100

    fix in number for board instad of bcm

--- rpi/controller.py
+++ rpi/controller.py
@@ -4,22 +4,24 @@ import RPi.GPIO as GPIO
 
 
 def gpio_on(channel,seconds):
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(channel, GPIO.OUT)
     start = int(time.time())
+    print("turning on")
     state = 1
     try:
         GPIO.output(channel, state)
         time.sleep(seconds)
+        print("turning off")
         state = 0
         GPIO.output(channel, state)
-    except:
+    except exception as e:
         GPIO.output(channel, 0)
         GPIO.cleanup()
         exit()
 
 def main():
-    gpio_on(37,30)
+    gpio_on(26,10)
 
 
 
