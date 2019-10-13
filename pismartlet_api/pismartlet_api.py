9d69a47c2892f84911a2a8a316d0a462ec2c30c9
James Rine <jlrine2@alaska.edu>
2019-10-13 11:35:33 -0800

    add write to json to postSchedule

--- pismartlet_api/pismartlet_api.py
+++ pismartlet_api/pismartlet_api.py
@@ -1,4 +1,8 @@
 import json
+
+import datetime
+from dateutil.parser import parse 
+
 from operator import itemgetter
 from typing import Dict, List
 
@@ -11,7 +15,6 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:port"}})
 
 
-
 @app.route("/getStatus", methods=['GET'])
 @cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
 def get_status():
@@ -21,15 +24,38 @@ def get_status():
 @app.route("/postStatus", methods=['POST'])
 @cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
 def post_status():
-    print(request.get_data())
+    data = request.get_json()
+    print(data)
     return "recived"
 
 
 @app.route("/scheduleEvent", methods=['POST'])
 @cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
 def schedule():
-    data = request.get_json()
-    print(data)
+    data = request.get_json()['data']
+    date = data['date']
+    time = data['time']
+    #pull hours minutes from duration
+    durration_time = data['duration']
+    durration = durration_time[11:16] #hours and minutes between 11 and 16
+    
+    #pull timestamp from time and date
+    start = date[:14] + time[:14] #date from date time from time
+    durration = start + stringtoseconds(durration)
+
+    #define start as epoch timestamp
+    start = datetimetoepoch(start)
+    #define end as epoch
+    end = start + durration
+
+    event = {
+        "start" : start,
+        "end" : end,
+        "status" : "pending"
+    }
+
+    append_json(event)
+    print(event)
     return jsonify(data)
 
 
@@ -71,10 +97,30 @@ def get_next_pending() -> Dict:
     data = filter_pending_data()
     return data[0]
 
+
 def get_next_three_pending() -> List:
     data = filter_pending_data()
     print(data[0:3])
     return data[0:3]
+
+
+def append_json(event):
+    with open (DATA, 'w') as json_file:
+        data = json.load(json_file)
+        data.append(event)
+        json_file.write(str(data))
+
+
+def datetimetoepoch(date_timestr)->int:
+    date_object = parse(date_timestr)
+    seconds = date_object.timestamp()
+    return seconds
+
+
+def stringtoseconds(time)->int:
+    hours = int(time[:2])*60*60
+    seconds = int(time[3:])*60
+    return (hours+seconds)
 
 
 
