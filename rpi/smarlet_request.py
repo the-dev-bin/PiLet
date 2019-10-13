70347ee8e7a033e3eb6f9b9f666598013ce04809
James Rine <jlrine2@alaska.edu>
2019-10-13 14:53:43 -0800

    debug

--- rpi/smarlet_request.py
+++ rpi/smarlet_request.py
@@ -17,7 +17,7 @@ def get_content() -> json:
 
 def send_status(status, start_time) -> None:
     data = {"status": status, "start": start_time}
-    requests.post(postURL, data=str(data))
+    requests.post(postURL, json=json.dumps(data))
 
 
 def main() -> None:
