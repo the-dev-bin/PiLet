209a30b0f617851e650e89af705f8d01a837149e
James Rine <jlrine2@alaska.edu>
2019-10-13 14:41:36 -0800

    debug

--- pismartlet_api/pismartlet_api.py
+++ pismartlet_api/pismartlet_api.py
@@ -25,7 +25,7 @@ def get_status():
 @cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
 def post_status():
     print('post')
-    data = request.get_json()
+    data = request.get_data()
     print(type(data)))
     start = data['start']
     status = data['status']
