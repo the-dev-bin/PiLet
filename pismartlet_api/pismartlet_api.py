109ef043edc8d9e5b0db539f2f718c9b573b0f55
James Rine <jlrine2@alaska.edu>
2019-10-13 10:12:09 -0800

    added sort yo getScedule to r return next 3 events

--- pismartlet_api/pismartlet_api.py
+++ pismartlet_api/pismartlet_api.py
@@ -1,4 +1,5 @@
 import json
+from operator import itemgetter
 from typing import Dict, List
 
 from flask import Flask, jsonify, request
@@ -10,36 +11,9 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:port"}})
 
 @app.route("/getStatus", methods=['GET'])
+@cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
 def get_status():
-    return json.dumps(get_next_pending())
-
-
-def get_data() -> List:
-    with open(DATA) as json_file:
-        data = json.load(json_file)
-    return data
-
-
-def filter_pending_data() -> List:
-    data = get_data()
-    pending = []
-    for event in data:
-        if event['status'] == 'pending':
-            pending.append(event)
-        else:
-            continue
-    return pending
-
-
-def get_next_pending() -> Dict:
-    data = filter_pending_data()
-    current = data[0]
-    print(current)
-    for event in data:
-        print(event['start'])
-        if int(event['start']) < int(current['start']):
-            current = event
-    return current
+    return jsonify(get_next_pending())
 
 
 @app.route("/postStatus", methods=['POST'])
@@ -55,17 +29,48 @@ def schedule():
 
 
 @app.route("/getSchedule", methods=['GET'])
+@cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
 def get_schedule():
-    return jsonify(get_data)
+    return jsonify(get_next_three_pending())
 
 
-@app.route("active", methods=['GET'])
+@app.route("/active", methods=['GET'])
+@cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
 def is_active():
     return
 
 @app.route('/test')
 def testing():
     return "<form action='/' method='POST'><input type='submit'></form>"
+
+def get_data() -> List:
+    with open(DATA) as json_file:
+        data = json.load(json_file)
+    return data
+
+
+def filter_pending_data() -> List:
+    data = get_data()
+    pending = []
+    for event in data:
+        if event['status'] == 'pending':
+            pending.append(event)
+        else:
+            continue
+    sorted_data = sorted(pending, key= itemgetter('start'))
+    print(sorted_data)
+    return sorted_data
+
+
+def get_next_pending() -> Dict:
+    data = filter_pending_data()
+    return data[0]
+
+def get_next_three_pending() -> List:
+    data = filter_pending_data()
+    print(data[0:3])
+    return data[0:3]
+
 
 
 if __name__ == "__main__":
