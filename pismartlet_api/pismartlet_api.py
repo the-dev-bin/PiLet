dcc3db591e71bdcb76c77a97f2da0e0783bdfd69
James Rine <jlrine2@alaska.edu>
2019-10-13 14:36:48 -0800

    debug

--- pismartlet_api/pismartlet_api.py
+++ pismartlet_api/pismartlet_api.py
@@ -26,7 +26,7 @@ def get_status():
 def post_status():
     print('post')
     data = request.get_json()
-    print(type(data))
+    print(type(dict(data)))
     start = data['start']
     status = data['status']
 
