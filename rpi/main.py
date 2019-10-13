378861a9e4eb73a3edae083de8294029d8c75274
James Rine <jlrine2@alaska.edu>
2019-10-13 14:11:04 -0800

    add send status calls to main

--- rpi/main.py
+++ rpi/main.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-from smarlet_request import get_content
+from smarlet_request import get_content, send_status
 from relay import get_state, gpio_off, gpio_on
 
 PIN = 26
@@ -18,10 +18,13 @@ def main() -> None:
     if time.time() >= start and time.time() <= start + 300:
         if get_state(PIN) == 0:
             gpio_on(PIN)
+            send_status(start, 'pending')
 
     if time.time() >= end and time.time() <= end + 300:
         if get_state(PIN) == 1:
             gpio_off(PIN)
+            send_status(start, 'done')
+
 
 
 if __name__ == "__main__":
