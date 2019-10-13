f13d0c736ad173482a5e8f732de3a25badd32b95
tylercchase <tylercchase@gmail.com>
2019-10-13 13:35:25 -0800

    Upcoming changes integration

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'reserve', loadChildren: './reserve/reserve.module#ReservePageModule' },
 ];
 
 @NgModule({
